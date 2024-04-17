f070c10145ec1836c0bec53766caf99c0c1c7ba4
olesiavoprykova <olesia.voprikova@yahoo.com>
2024-04-17 20:29:54 +0200

    update

--- src/main/java/com/phonebook/utils/DataProviders.java
+++ src/main/java/com/phonebook/utils/DataProviders.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class DataProviders {
     @DataProvider
-    public Iterator<Object[]> addNewContact(){//этот тест в ДЗ не далать
+    public Iterator<Object[]> addNewContact(){
         List<Object[]> list = new ArrayList<>();
         list.add(new Object[]{"Anna", "Müller", "1234567890", "annamuel@gmail.com", "Münich","Co-worker"});
         list.add(new Object[]{"Anna1", "Müller", "1234567890", "annamuel@gmail.com", "Münich","Co-worker"});
@@ -29,8 +29,13 @@ public class DataProviders {
         String line = reader.readLine();
         while (line != null) {
             String[] split = line.split(",");
-            list.add(new Object[]{new Contact().setName(split[0]).setLastName(split[1]).setPhone(split[2])
-                    .setEmail(split[3]).setAddress(split[4]).setDescription(split[5])});
+            list.add(new Object[]{new Contact()
+                            .setName(split[0])
+                            .setLastName(split[1])
+                            .setPhone(split[2])
+                            .setEmail(split[3])
+                            .setAddress(split[4])
+                            .setDescription(split[5])});
             line = reader.readLine();
         }
         return list.iterator();
